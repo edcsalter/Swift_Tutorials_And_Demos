7583c87ae7a2e095b8491dcbd1ebf4d53ceb05ad
MadArkitekt <edwardcsalter@gmail.com>
2014-10-25 23:46:21 -0400

    Initial Push of WaveformAudioTut

---
+++ WaveformAudioTut/WaveformAudioTut/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WaveformAudioTut
+//
+//  Created by Edward Salter on 10/25/14.
+//  Copyright (c) 2014 Ed Salter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
