3ce826e52c71c0f216e7bb1031e8b9a4948b94d3
Ed Salter <madarkitekt@gmail.com>
2018-06-12 07:29:00 -0400

    Add Data Structures & Algorithms
    - Linked List

---
+++ Data Structures & Algorithms/DataStructures.playground/Contents.swift
@@ -0,0 +1,52 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import Foundation
+
+public class Node<Value> {
+    public var value: Value
+    public var next: Node?
+    
+    public init(value: Value, next: Node? = nil) {
+        self.value = value
+        self.next = next
+    }
+}
+
+extension Node: CustomStringConvertible {
+    public var description: String {
+        guard let next = next else {
+            return "\(value)"
+        }
+        return "\(value) -> " + String(describing: next) + " "
+    }
+}
+
+let node1 = Node(value: 1)
+let node2 = Node(value: 2)
+let node3 = Node(value: 3)
+
+node1.next = node2
+node2.next = node3
+
+print(node1)
+
+public class LinkedList<Value> {
+    public var head: Node<Value>?
+    public var tail: Node<Value>?
+    
+    public init() { }
+    
+    public var isEmpty: Bool {
+        return head == nil
+    }
+}
+
+extension LinkedList: CustomStringConvertible {
+    public var description: String {
+        guard let head = head else {
+            return "Empty List"
+        }
+        return String(describing: head)
+    }
+}
