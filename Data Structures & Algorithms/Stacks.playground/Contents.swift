7c015590160d6591e1b1afa5f3e2049585e3bf91
Ed <edcsalter@gmail.com>
2020-09-01 21:05:06 -0400

    Stacks Commenting

--- Data Structures & Algorithms/Stacks.playground/Contents.swift
+++ Data Structures & Algorithms/Stacks.playground/Contents.swift
@@ -1,3 +1,32 @@
 // Copyright (c) 2019 Razeware LLC
 // For full license & permission details, see LICENSE.markdown.
 
+// Defining backing storage type for Stack: an array
+// Array chosen because of constant time insertions and deletions at one end
+// via 'append' and 'popLast' which will also make the stacks LIFO.
+
+public struct Stack<Element> {
+    private var storage: [Element] = []
+    
+    public init() {}
+}
+
+extension Stack: CustomStringConvertible {
+    /**
+    EXPLANATION:
+        Create array mapping elements to string:
+            storage.map { "\($0)"}
+     
+        Reverse the previous array:
+            reversed()
+     
+        Flatten array into string with new line inbetween each element
+    */
+    public var description: String {
+        """
+        ---top---
+        \(storage.map { "\($0)"}.reversed().joined(separator: "\n"))
+        ---------
+        """
+    }
+}
